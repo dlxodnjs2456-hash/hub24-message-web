ac2020fc8ebcf0599ddb0500de020ef9daff881a
dlxodnjs2456-hash <dlxodnjs2456@gmail.com>
2026-08-24 19:58:12 +0900

    Add web root layout

---
+++ app/layout.js
@@ -0,0 +1,7 @@
+import './globals.css';
+
+export const metadata={title:'HUB24 MESSAGE',description:'Telegram messaging operations dashboard'};
+
+export default function RootLayout({children}){
+  return <html lang="ko"><body>{children}</body></html>;
+}
