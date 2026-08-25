d63ac4bd8d6d99c7519d7695b6e1cb658963f2f8
dlxodnjs2456-hash <dlxodnjs2456@gmail.com>
2026-08-25 13:07:59 +0900

    Add HUB24 global top navigation

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css';
+import TopNav from './topnav';
 
 export const metadata={title:'HUB24 MESSAGE',description:'Telegram messaging operations dashboard'};
 
 export default function RootLayout({children}){
-  return <html lang="ko"><body>{children}</body></html>;
+  return <html lang="ko"><body><TopNav/>{children}</body></html>;
 }
